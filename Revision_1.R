16799e30f63d71456b38d223667a57c58fc5cfc3
Patrick Turko <patrick.turko@gmail.com>
2017-10-18 12:07:04 +0200

    Add graph based on percentage data

--- Revision_1.R
+++ Revision_1.R
@@ -24,12 +24,19 @@ counts <- dcast(matched, date + type ~ mlg, fun.aggregate = length)
 counts <- melt(counts, id.vars = c("date", "type"))
 names(counts) <- c("date", "type", "mlg", "count")
 
+#Express counts as percentages instead.
+counts <- ddply(counts, .(date, type), function(x){
+  total <- sum(x$count)
+  x$percent <- x$count / total
+  x
+})
+
 #Do a Fisher's exact test to check whether infected and random samples differ at each date
 dates <- unique(counts$date)
 pop.fish <- data.frame(date = dates, p_value = 0)
 
 for(i in 1:length(dates)){
-  tmp <- counts[counts$date == dates[i],] 
+  tmp <- counts[counts$date == dates[i], 1:4] 
   tmp <- dcast(tmp, mlg ~ type)
   tmp <- tmp[tmp$Infected != 0 | tmp$Random !=0, ]
   fish <- fisher.test(as.matrix(tmp[, 2:3]), workspace = 1e+09)
@@ -38,9 +45,9 @@ for(i in 1:length(dates)){
 
 ####Make a graph showing how sample types differ at each date
 # First, rename the mlgs so that each date has it's own set
-
-mlgs_by_date <- ddply(counts, .(date), function(x){
-  tmp <- dcast(x, mlg ~ type)
+# For the percent data
+percent_by_date <- ddply(counts, .(date), function(x){
+  tmp <- dcast(x, mlg ~ type, value.var = "percent")
   tmp <- tmp[tmp$Infected != 0 | tmp$Random !=0, ]
   tmp$mlg2 <- paste(x$date[1], tmp$mlg, sep = "_")
   tmp <- tmp[order(tmp$Random), ]
@@ -48,13 +55,29 @@ mlgs_by_date <- ddply(counts, .(date), function(x){
   tmp
 })
 
-mlgs_by_date <- melt(mlgs_by_date, id.vars = c("date", "mlg", "mlg2"))
-mlgs_by_date$type <- factor(mlgs_by_date$type, levels = c("Random", "Infected"))
-names(mlgs_by_date)[4:5] <- c("type", "count")
-mlgs_by_date[mlgs_by_date == 0] <- NA
+#And for the count data
+counts_by_date <- ddply(counts, .(date), function(x){
+  tmp <- dcast(x, mlg ~ type, value.var = "count")
+  tmp <- tmp[tmp$Infected != 0 | tmp$Random !=0, ]
+  tmp$mlg2 <- paste(x$date[1], tmp$mlg, sep = "_")
+  tmp <- tmp[order(tmp$Random), ]
+  tmp$mlg2 <- factor(tmp$mlg2, levels = tmp$mlg2)
+  tmp
+})
 
-#Second, create the graph 
-pop.graph <- ggplot(mlgs_by_date, 
+# Refactor variables to get the desired order in the plot
+percent_by_date <- melt(percent_by_date, id.vars = c("date", "mlg", "mlg2"))
+names(percent_by_date)[4:5] <- c("type", "percent")
+percent_by_date$type <- factor(percent_by_date$type, levels = c("Random", "Infected"))
+percent_by_date[percent_by_date == 0] <- NA
+
+counts_by_date <- melt(counts_by_date, id.vars = c("date", "mlg", "mlg2"))
+names(counts_by_date)[4:5] <- c("type", "count")
+counts_by_date$type <- factor(counts_by_date$type, levels = c("Random", "Infected"))
+counts_by_date[counts_by_date == 0] <- NA
+
+#Second, create the graph (for counts)
+pop.graph <- ggplot(counts_by_date, 
                     aes(x = mlg2, y = count)) +
   facet_grid(date~type, scales = "free") +
   geom_bar(stat = "identity", width = 0.8) +
@@ -67,7 +90,26 @@ pop.graph <- ggplot(mlgs_by_date,
   xlab(label = "MLG") +
   theme(aspect.ratio = 1) +
   coord_flip()
+pop.graph
 
-ggsave("Rand_vs_Inf.pdf", pop.graph, height = 10, width = 4, units = "in", dpi = 600)
+# ggsave("Rand_vs_Inf.pdf", pop.graph, height = 10, width = 4, units = "in", dpi = 600)
 
-       
+#for percents
+pop.graph2 <- ggplot(percent_by_date, 
+                    aes(x = mlg2, y = percent * 100)) +
+  facet_grid(date~type, scales = "free_y") +
+  geom_bar(stat = "identity", width = 0.8) +
+  theme_bw() +
+  removeGrid() +
+  scale_x_discrete(expand = c(0.02, 0.02)) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  ylab(label = "Percent of Sample") +
+  xlab(label = "MLG") +
+  theme(aspect.ratio = 1) +
+  coord_flip() +
+  geom_hline(yintercept = 5, size = 0.1)
+pop.graph2
+
+ggsave("Rand_vs_Inf_percent.pdf", pop.graph2, height = 10, width = 4, units = "in", dpi = 600)
+
